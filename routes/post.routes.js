d61b1949eb5898b594703f601a14639aa0c2e580
zapatran <zapatran@users.noreply.github.com>
2021-04-30 15:53:26 +0200

    update

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -1,21 +1,58 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
-const User = require("../models/User.model");
-const Post = require("../models/Post.model");
+const User = require('../models/User.model');
+const Post = require('../models/Post.model');
 
 // ****************************************************************************************
 // GET route to display the form to create a new post
 // ****************************************************************************************
 
 // localhost:3000/post-create
-router.get("/post-create", (req, res) => {
+router.get('/post-create', (req, res) => {
   User.find()
     .then((dbUsers) => {
-      res.render("posts/create", { dbUsers });
+      res.render('posts/create', { dbUsers: dbUsers });
     })
-    .catch((err) => console.log(`Err while displaying post input page: ${err}`));
+    .catch((err) =>
+      console.log(`Err while displaying post input page: ${err}`)
+    );
 });
 
+router.post('/post-create', (req, res, next) => {
+  // const title = req.body.title;
+  // const content = req.body.content;
+  // const author = req.body.author;
+
+  const { title, content, author } = req.body;
+
+  // Post.create({
+  //   title: title,
+  //   content: content,
+  //   author: author,
+  // })
+  Post.create({ title, content, author })
+    .then((dbPost) => {
+      return User.findByIdAndUpdate(author, { $push: { posts: dbPost._id } });
+    })
+    .then(() => {
+      res.redirect('/posts');
+    })
+    .catch((error) => {
+      next(error);
+    });
+});
+
+router.get('/posts', (req, res) => {
+  Post.find()
+    .populate('author', { username: 1 })
+    .then((dbPosts) => {
+      res.render('posts/list', { posts: dbPosts });
+    })
+
+    .catch((error) => {
+      next(error);
+    });
+});
 // ****************************************************************************************
 // POST route to submit the form to create a post
 // ****************************************************************************************
